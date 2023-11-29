33263b8c22172aa7686fbcdeecd800c1c38b40e4
Oleg Seifullin <sensei.lis@yandex.com>
2023-11-29 23:38:50 +0200

    Домашнее задание 2-го семинара

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,25 +1,17 @@
 ﻿/*Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.*/
-using System;
 
-class Program
+Console.Write("Введите целое число из отрезка [10, 99]: ");
+int inputNumber = int.Parse(Console.ReadLine());
+
+if (inputNumber < 10 || inputNumber > 99)
 {
-   static void Main()
-   {
-      Console.WriteLine("Введите целое число из отрезка [10, 99]: ");
-
-      // Ввод числа с клавиатуры
-      int number;
-      while (true)
-      {
-         if (int.TryParse(Console.ReadLine(), out number) && number >= 10 && number <= 99)
-            break;
-         else
-            Console.WriteLine("Введите корректное число из отрезка [10, 99]: ");
-      }
-
-      // Находим наибольшую цифру
-      int maxDigit = number % 10 > number / 10 ? number % 10 : number / 10;
-
-      Console.WriteLine($"Наибольшая цифра в числе {number} - {maxDigit}");
-   }
+   Console.WriteLine("Число вне допустимого диапазона.");
+   return;
 }
+
+int tensDigit = inputNumber / 10;
+int onesDigit = inputNumber % 10;
+
+int maxDigit = Math.Max(tensDigit, onesDigit);
+
+Console.WriteLine($"Наибольшая цифра числа {inputNumber}: {maxDigit}");
