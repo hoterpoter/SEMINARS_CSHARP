98b68e36b1c5055182ae5dc0dbd66d0b0edd2c2e
Oleg Seifullin <sensei.lis@yandex.com>
2023-11-29 20:50:59 +0200

    Домашнее задание к семинару Массивы03

---
+++ Task_06/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] array = { 54.56, 22.76, 23.43, 23.54, 25.43, 56.45, 67.23 };
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+   if (array[i] < min)
+   {
+      min = array[i];
+   }
+   else if (array[i] > max)
+   {
+      max = array[i];
+   }
+}
+
+double difference = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {difference}");
